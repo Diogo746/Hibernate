0f944b64899b83d5f7985f0296b2df4b561315ed
Diogo <diogo.barbosa@edu.pe.senac.br>
2025-04-22 16:53:50 -0300

    Add files via upload

---
+++ meu-projeto/src/main/java/com/exemplo/TesteHibernate.java
@@ -0,0 +1,29 @@
+package com.exemplo;
+
+import com.exemplo.modelo.Pessoa;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TesteHibernate {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        Pessoa p = new Pessoa();
+        p.setNome("Pedro");
+        p.setIdade(24);
+
+        session.beginTransaction();
+        session.save(p);
+        session.getTransaction().commit();
+
+        session.close();
+        factory.close();
+
+        System.out.println("Pessoa salva com sucesso!");
+    }
+
+}
+
+
